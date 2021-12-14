8eb9cd33e1ab4f4de0d67aa5494ff440554c2bc6
Rajesh G <coding3601@gmail.com>
2021-12-14 21:07:44 +0530

    signin signup route added in backend
    Co-authored-by: SRI JAYAN E <SriJayan17@users.noreply.github.com>

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,69 +1,72 @@
 const bcrypt = require('bcryptjs');
-const { User } = require("../models/UserModel");
+const User = require("../models/UserModel");
 const jwt = require('jsonwebtoken');
 
 exports.signin = async (req, res, next) => {
-    // const email = req.body.email;
-    // const password = req.body.password;
+    const email = req.body.email;
+    const password = req.body.password;
 
-    // try {
-    //     if(email.trim().length==0 || password.trim().length==0){
-    //         const emptyErr = new Error("Enter required fileds");
-    //         emptyErr.error = {};
-    //         if(email.trim.length==0)
-    //             emptyErr.error.email="Email is required";
-    //         if(password.trim().length == 0) {
-    //             emptyErr.error.password = "Password is required";
-    //         }
-    //         throw emptyErr;
-    //     }
-        
-    //     if(!email.contains("@")){
-    //         const invalidEmailErr = new Error("Invalid Email");
-    //         invalidEmailErr.error = {
-    //             email:"Email is invalid"
-    //         };
-    //         throw invalidEmailErr;
-    //     }
-    //     const userfound = await User.findOne({ email });   
-        
-    //     if(!userfound){
-    //         const userExistErr = new Error("User Not found");
-    //         userExistErr.error = {
-    //             email:"User not found"
-    //         };
-    //         throw userExistErr;
-    //     }
-    //     const hashedPassword = userfound.password;
-    //     const isEqual = await bcrypt.compare(password,hashedPassword);
-    //     if(!isEqual){
-    //         const passwordErr = new Error("Password doesn't match");
-    //         passwordErr.error = {
-    //             email:"Password doesn't match"
-    //         };
-    //         throw passwordErr;
-    //     }
-        
-        
-    //     const token = jwt.sign(
-    //         { email: user.email },
-    //         "ewgeiuliefcuhlorehciwfeycrgry",
-    //         { expiresIn: "1h" }
-    //     );
+    if(email.trim().length==0 || password.trim().length==0){
+        const emptyErr = new Error("Enter required fileds");
+        emptyErr.error = {};
+        if(email.trim.length==0)
+            emptyErr.error.email="Email is required";
+        if(password.trim().length == 0) {
+            emptyErr.error.password = "Password is required";
+        }
+        throw emptyErr;
+    }
 
-    //     return res.status(200).json({
-    //         message: "Account created successfully",
-    //         body: {
-    //             username: isCreated.email,
-    //             token
-    //         }
-    //     })
+    if(!email.includes("@")){
+        const invalidEmailErr = new Error("Invalid Email");
+        invalidEmailErr.error = {
+            email:"Email is invalid"
+        };
+        throw invalidEmailErr;
+    }
 
-    // } catch(err) {
-    //     if(!err.status) err.status = 500;
-    //     next(err);
-    // }
+    let ourUser;
     
+    User.findOne({ email })
+    .then(userfound => {
+        ourUser = userfound;
+        if(!userfound){
+            const userExistErr = new Error("User Not Found");
+            userExistErr.error = {
+                email:"User Not Found"
+            };
+            throw userExistErr;
+        }
+        return bcrypt.compare(password, userfound.password);
+    })
+    .then(isEqual => {
+        if(!isEqual){
+            const signIpErr = new Error("Password doesn't match!");
+            signIpErr.error = {
+                email:"Password doesn't match!"
+            };
+            throw signIpErr;        
+        }
+        
+        const token = jwt.sign(
+            { email: ourUser.email },
+            "ewgeiuliefcuhlorehciwfeycrgry",
+            { expiresIn: "1h" }
+        );
+
+        return res.status(200).json({
+            message: "Account created successfully",
+            body: {
+                username: ourUser.email,
+                token
+            }
+        })
+        
+    })
+    .catch(err => {
+        if(!err.status) err.status = 500;
+        next(err); 
+    })
 };
 
 exports.signup = (req,res,next) => {
@@ -88,34 +91,22 @@ exports.signup = (req,res,next) => {
         };
         throw invalidEmailErr;
     }
-    User.find()
-    .then(users => {
-        users.forEach(user=>{
-            if(user.email===email){
-                const userExistErr = new Error("Email already exist");
-                userExistErr.error = {
-                    email:"Email alreadty exist"
-                };
-                throw userExistErr;
-            }
-        })
+    
+    User.findOne({ email })
+    .then(userfound => {
+        if(userfound){
+            const userExistErr = new Error("Email already exist");
+            userExistErr.error = {
+                email:"Email alreadty exist"
+            };
+            throw userExistErr;
+        }
+
         return bcrypt.hash(password, 12);
     })
-    // User.findOne({ email })
-    // .then(userfound => {
-    //     console.log(userfound);
-    //     if(userfound){
-    //         const userExistErr = new Error("Email already exist");
-    //         userExistErr.error = {
-    //             email:"Email alreadty exist"
-    //         };
-    //         throw userExistErr;
-    //     }
-
-    //     return bcrypt.hash(password, 12);
-    // })
-    .then(hashedPassword=>{
-        const newUser = new User({email,password});
+    .then(hashedPassword => {
+        console.log(hashedPassword);
+        const newUser = new User({email, password:hashedPassword});
         return newUser.save();
     })
     .then(isCreated=>{
@@ -128,10 +119,12 @@ exports.signup = (req,res,next) => {
         }
 
         const token = jwt.sign(
-            { email: user.email },
+            { email: isCreated.email },
             "ewgeiuliefcuhlorehciwfeycrgry",
             { expiresIn: "1h" }
         );
+
+        console.log(isCreated);
 
         return res.status(200).json({
             message: "Account created successfully",
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -11,6 +11,6 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model("user",userSchema);
+const User = mongoose.model("user", userSchema);
 
-module.exports = {User};
+module.exports = User;
